4b63f84d0fac9b0e4a8e9c38a6c196201b17782a
cbptamtom <cbptamcode@gmail.com>
2024-05-09 22:12:28 +0700

    auth

---
+++ src/components/layout/current-user.tsx
@@ -0,0 +1,17 @@
+import { Popover } from "antd";
+import React from "react";
+
+const CurrentUser = () => {
+  return (
+    <>
+      <Popover
+        placement="bottomRight"
+        trigger="click"
+        overlayStyle={{ zIndex: 999 }}
+        overlayInnerStyle={{ padding: 0 }}
+      ></Popover>
+    </>
+  );
+};
+
+export default CurrentUser;
